9dde1cbb3836b3dc217037a99eabd3c5ce1b650c
Michael Degli-Angeli <104280483+supertables@users.noreply.github.com>
2024-08-14 21:02:32 -0400

    release ca.beta @0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Holidex.MixProject do
   def project do
     [
       app: :holidex,
-      version: "0.1.1",
+      version: "0.1.2",
       description: description(),
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
